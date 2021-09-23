8ca47c7a3b134a2f7f6994af5829f1f5a6356ada
farras.f.interns <farras.f.interns@aux.gojek.com>
2021-09-23 09:12:05 +0700

    test: handle getLength of horizontal line

--- app/src/main/java/com/geometry/Line.java
+++ app/src/main/java/com/geometry/Line.java
@@ -1,11 +1,15 @@
 package com.geometry;
 
 public class Line {
+    private final int x1;
+    private final int x2;
 
     public Line(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.x2 = x2;
     }
 
     public double getLength() {
-        return 0;
+        return Math.abs(x1 - x2);
     }
 }
--- app/src/test/java/com/geometry/LineTest.java
+++ app/src/test/java/com/geometry/LineTest.java
@@ -5,11 +5,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class LineTest {
+    private static final double EPS = 1e-9;
 
     @Test
     void coincidePointsShouldReturnZeroLength() {
         Line line = new Line(1, 1, 1, 1);
         double length = line.getLength();
-        assertEquals(0, length, 0.0001);
+        assertEquals(0, length, EPS);
+    }
+
+    @Test
+    void horizontalLineShouldReturnCorrectLength() {
+        Line line = new Line(2, 1, 3, 1);
+        double length = line.getLength();
+        assertEquals(1, length, EPS);
     }
 }
